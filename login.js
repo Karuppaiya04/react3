0aaa635de5dac31ddd382a315ce9c10d7d9c2e99
Kp <146843355+Karuppaiya04@users.noreply.github.com>
2024-04-10 16:48:28 +0530

    login
    project

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username === 'user' && password === 'password') {
+        alert('Login successful!');
+    } else {
+        alert('Incorrect username or password. Please try again.');
+    }
+});
